a4af15e018c58dd123b6247578fc2399fc2ede02
Ivan <shadowhuntercismail@gmail.com>
2016-11-02 18:19:34 +0200

    Adding project

---
+++ models/Book.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%book}}".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $author
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%book}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'author'], 'required'],
+            [['title', 'author'], 'string', 'max' => 64],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'author' => 'Author',
+        ];
+    }
+}
